a8984267ce2678a682dc272d14eb94b289e4b615
Dario Gabriel <dario.gabriel@atsinformatica.com.br>
2024-11-24 17:53:45 -0300

    Cria  conexão com banco e refatoração de código

--- server.js
+++ server.js
@@ -1,42 +1,9 @@
 import express from "express";
-
-const posts = [
-    {
-        id: 1,
-        descricao: "Uma foto teste",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 2,
-        descricao: "Gato brincando com um novelo de lã",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 3,
-        descricao: "Paisagem com pôr do sol",
-        imagem: "https://placecats.com/millie/300/150"
-    }
-];
+import routes from "./src/routes/postRoutes.js";
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorId(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id);
-    res.status(200).json(posts[index]);
 });
